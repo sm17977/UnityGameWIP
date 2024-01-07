b065400b08d57abe68e08a4ccffec16963de04e8
Sean <44192194+sm17977@users.noreply.github.com>
2024-01-07 11:35:25 +0000

    Fixed ranged indicator error

--- Assets/Scripts/RangeIndicator_Controller.cs
+++ Assets/Scripts/RangeIndicator_Controller.cs
@@ -11,16 +11,18 @@ public class RangeIndicator_Controller : MonoBehaviour
 
     // Keep radius 0.5 so the range indicator circle fills the quad
     private float radius = 0.5f;
-    private Lux_Player_Controller playerScript;
+    private Lux_Player_Controller playerController;
 
     void Start(){
-        playerScript = player.GetComponent<Lux_Player_Controller>();
-        size = playerScript.GetAttackRange();
+        Lux lux = new Lux();
+        playerController = player.GetComponent<Lux_Player_Controller>();
+        size = lux.AA_range;
     }
 
 
     // Update is called once per frame
     void Update(){
+        
         if(AARangeIndicatorMaterial != null){
             AARangeIndicatorMaterial.SetFloat("_radius", radius);
             AARangeIndicatorMaterial.SetFloat("_borderWidth", borderWidth);
